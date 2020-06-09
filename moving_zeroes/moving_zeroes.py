18f0703ed0179e5c83b96e73fc5fc3c0a0b2e12b
Dave Kane <davelkane@gmail.com>
2020-06-08 22:49:29 -0500

    single_num and moving_zeroes complete

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,14 +1,41 @@
-'''
+"""
 Input: a List of integers
 Returns: a List of integers
-'''
+"""
+"""
+    Create two pointers, one for the position being evaluated, one for the end of iteration
+    Iterate through the list
+    If value is 0:
+         remove from list
+         append to the end
+         Decrement end postion
+         Do not advance the search position pointer
+    Otherwise:
+        Advance the the search position pointer
+    When end poistion is reached, return
+
+"""
+
+
 def moving_zeroes(arr):
-    # Your code here
+    # Create a pointer for the search and end position
+    i = 0
+    end = len(arr) - 1
+    # Iterate through the list, while the search position has not reached the end postion
+    while i < end:
+        # If the value is 0, append value, del postion, dec end
+        if arr[i] == 0:
+            arr.append(arr[i])
+            del arr[i]
+            end -= 1
+        # Inc search postion
+        else:
+            i += 1
+    # Return adjusted list
+    return arr
 
-    pass
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
 
